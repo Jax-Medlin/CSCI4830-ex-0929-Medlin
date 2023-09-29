e1556e8dbc5d5e6c06b71de583d190894b98be20
Jax-Medlin <54479037+Jax-Medlin@users.noreply.github.com>
2023-09-29 14:49:20 -0500

    Add files via upload

--- batch-process-Medlin.sh
+++ batch-process-Medlin.sh
@@ -1,9 +1,10 @@
 #! /bin/sh 
 for f in "$1"/*; do
   if [ -f "$f" ]; then
+    WORD="$(wc -w "${f}" | cut -d ' ' -f1)"
     SIZE="$(du -sh "${f}" | cut -f1)"
     echo "Processing $f file..."
     echo "$SIZE"
+    echo "$WORD"
   fi
 done
-
